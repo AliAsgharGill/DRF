f5b01ac7d08a981cd36864b61b74770fc96a1b28
AliAsgharGill <stylinalivlogs@gmail.com>
2024-09-20 13:33:41 +0500

    FUNCTIONS BASED CRUD WORKING

--- API/myapp/urls.py
+++ API/myapp/urls.py
@@ -4,7 +4,7 @@ from .views import Get_API, Post_API, Put_API, Patch_API, Delete_API, Get_Studen
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('GET/', Get_API),
-    path("GET_STUDENT/<int:id>/",Get_Student),
+    path("GET/<int:id>/",Get_Student),
     path('POST/', Post_API),
     path("PUT/<int:id>/",Put_API),
     path("PATCH/<int:id>/",Patch_API),
